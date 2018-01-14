8e2df6114b8eac530fa885093e8d6e660140fbd2
nayan5565 <nayan5565@gmail.com>
2018-01-14 18:57:35 +0600

    init

---
+++ app/src/main/java/com/example/dev/chatapptuitorial/ChatMessage.java
@@ -0,0 +1,46 @@
+package com.example.dev.chatapptuitorial;
+
+import java.util.Date;
+
+/**
+ * Created by Dev on 1/14/2018.
+ */
+
+public class ChatMessage {
+    private String messageText, messageUser;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessage() {
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
